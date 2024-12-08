61cf55a14ad1ad14168352f64fd251163ca608e9
Rahul Kiran Gaddam <gaddam.rahul@gmail.com>
2024-12-08 22:53:23 +0530

    Initial Draft

--- components/app-footer/app-footer.js
+++
@@ -1,9 +0,0 @@
-import { BaseComponent } from '../base/base.js';
-
-class AppFooter extends BaseComponent {
-    constructor() {
-        super('app-footer');
-    }
-}
-
-customElements.define('app-footer', AppFooter);
--- components/app-header/app-header.js
+++
@@ -1,9 +0,0 @@
-import { BaseComponent } from '../base/base.js';
-
-class AppHeader extends BaseComponent {
-    constructor() {
-        super('app-header');
-    }
-}
-
-customElements.define('app-header', AppHeader);
